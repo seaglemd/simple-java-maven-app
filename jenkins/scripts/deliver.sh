638cdf6e6ba98ab75bec7d7b0062ecdf1ecad6ac
Matthew Seagle <matthew.seagle@meresecure.com>
2018-07-23 13:29:22 -0700

    updated the jenkins shell deliver file

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -5,7 +5,7 @@ echo 'into the local Maven repository, which will ultimately be stored in'
 echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
 echo 'volume).'
 set -x
-mvn jar:jar install:install help:evaluate -Dexpression=project.name
+/usr/local/apache-maven/apache-maven-3.5.4/bin/mvn jar:jar install:install help:evaluate -Dexpression=project.name
 set +x
 
 echo 'The following complex command extracts the value of the <name/> element'

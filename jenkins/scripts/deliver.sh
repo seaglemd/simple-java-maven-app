4e947d65c928d5acb0ecac537bfb3954d8e4145b
Matthew Seagle <matthew.seagle@meresecure.com>
2018-07-24 14:32:31 -0700

    next name try

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
 VERSION=$(mvn help:evaluate -Dexpression=project.version | grep -o "[0-9].[0-9]-SNAPSHOT$")
-NAME=$(mvn help:evaluate -Dexpression=project.name | grep -o "my-app")
 set -x
-name=$NAME | grep -o "my-app$"
-echo $name
-java -jar target/$name-$VERSION.jar
+java -jar target/my-app-$VERSION.jar

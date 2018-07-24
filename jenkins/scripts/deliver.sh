19e5ba1bfd12da0a513ca4c0f8d8380dcd52ae39
Matthew Seagle <matthew.seagle@meresecure.com>
2018-07-24 14:45:08 -0700

    still trying to only extract the match

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -2,6 +2,6 @@
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
 VERSION=$(mvn help:evaluate -Dexpression=project.version | grep -o "[0-9].[0-9]-SNAPSHOT$")
 set -x
-build_version=$VERSION | grep -o "[0-9].[0-9]-SNAPSHOT$"
+echo $VERSION | grep -o "[0-9].[0-9]-SNAPSHOT$"
 echo $build_version
 java -jar target/my-app-$build_version.jar

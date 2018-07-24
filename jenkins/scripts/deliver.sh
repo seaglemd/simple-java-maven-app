78f0d20f0e4b20ac7c781104022b17a757dd2689
Matthew Seagle <matthew.seagle@meresecure.com>
2018-07-24 14:15:29 -0700

    version and name

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -3,6 +3,7 @@ mvn jar:jar install:install help:evaluate -Dexpression=project.name
 VERSION=$(mvn help:evaluate -Dexpression=project.version | grep -o "[0-9].[0-9]-SNAPSHOT$")
 NAME=$(mvn help:evaluate -Dexpression=project.name | grep -o "my-app")
 echo "BEFORE THE ECHO ---------------"
+echo $VERSION
 echo $NAME
 echo "AFTER THE ECHO ----------------"
 NAME=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.name}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)

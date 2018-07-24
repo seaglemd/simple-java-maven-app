921706856b7fa9cb6c5cbf8462631cdf90aaef89
Matthew Seagle <matthew.seagle@meresecure.com>
2018-07-24 14:49:38 -0700

    I still don't know why it's not expanding

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -2,4 +2,5 @@
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
 VERSION=$(mvn help:evaluate -Dexpression=project.version | grep -o "[0-9].[0-9]-SNAPSHOT$")
 set -x
-java -jar target/my-app-${$VERSION | grep -o "[0-9].[0-9]-SNAPSHOT$"}.jar
+build_number=${$VERSION | grep -o "[0-9].[0-9]-SNAPSHOT$"}
+java -jar target/my-app-$build_number.jar

3695f265c8eff130c64bca941004b1cdc5398b56
Matthew Seagle <matthew.seagle@meresecure.com>
2018-07-24 14:57:50 -0700

    something is funky with variable assignment and declaration, so testing that

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -2,5 +2,7 @@
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
 VERSION=$(mvn help:evaluate -Dexpression=project.version | grep -o "[0-9].[0-9]-SNAPSHOT$")
 set -x
+build_number="blahblahblah"
+echo $build_number
 build_number=$($VERSION | grep -o "[0-9].[0-9]-SNAPSHOT$")
 java -jar target/my-app-$build_number.jar

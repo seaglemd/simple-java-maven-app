495fa0ddfe12df42aed1e0774273057c70e106a5
Matthew Seagle <matthew.seagle@meresecure.com>
2018-07-24 13:47:56 -0700

    more grepping added

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -3,7 +3,7 @@ mvn jar:jar install:install help:evaluate -Dexpression=project.name
 VERSION=$(mvn help:evaluate -Dexpression=project.version | grep "[0-9].[0-9]-SNAPSHOT$")
 $(mvn help:evaluate -Dexpression=project.version | grep "[0-9].[0-9]-SNAPSHOT$" | grep "[0-9].[0-9]-SNAPSHOT$")
 echo "BEFORE THE ECHO ---------------"
-echo $VERSION
+echo $VERSION | grep "[0-9].[0-9]-SNAPSHOT$"
 echo "AFTER THE ECHO ----------------"
 NAME=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.name}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
 java -jar target/my-app-1.0-SNAPSHOT.jar

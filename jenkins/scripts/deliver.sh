2d72e608aa9e4019270d9028b7edb4cbac523f87
Matthew Seagle <matthew.seagle@meresecure.com>
2018-07-24 13:39:08 -0700

    changing the way the command is run

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
-
-VERSION='mvn help:evaluate -Dexpression=project.version | grep "[0-9].[0-9]-SNAPSHOT$"'
+VERSION=$(mvn help:evaluate -Dexpression=project.version | grep "[0-9].[0-9]-SNAPSHOT$")
 echo $VERSION
-echo "NEXT VERSION -----------------------"
-${VERSION}
 NAME=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.name}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
 java -jar target/my-app-1.0-SNAPSHOT.jar
 

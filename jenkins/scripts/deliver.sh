1962405fc50093c1e5e3c84a40ea399d0fd0affd
Matthew Seagle <matthew.seagle@meresecure.com>
2018-07-24 12:24:10 -0700

    more script modifications to try and determine if the variables are being tampered with by the withMaven plugin

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -2,7 +2,7 @@
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
 NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
 VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
-java -version
-echo ${NAME}-${VERSION}.jar
-java -jar target/${NAME}-${VERSION}.jar
+echo '${NAME}:'
+echo ${NAME}
+#java -jar target/${NAME}-${VERSION}.jar
 

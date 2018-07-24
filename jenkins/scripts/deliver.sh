412f52eb84615276927072860cfe22578cfeb78b
Matthew Seagle <matthew.seagle@meresecure.com>
2018-07-24 14:27:14 -0700

    extracting end

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -3,6 +3,6 @@ mvn jar:jar install:install help:evaluate -Dexpression=project.name
 VERSION=$(mvn help:evaluate -Dexpression=project.version | grep -o "[0-9].[0-9]-SNAPSHOT$")
 NAME=$(mvn help:evaluate -Dexpression=project.name | grep -o "my-app")
 set -x
-name=$NAME | grep -o "my-app"
+name=$NAME | grep -o "my-app$"
 echo $name
 java -jar target/$NAME-$VERSION.jar

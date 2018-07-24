ff64c9b4718ee9178022cd4164eb358d6ef35a75
Matthew Seagle <matthew.seagle@meresecure.com>
2018-07-24 14:34:18 -0700

    back to versioning

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -2,4 +2,4 @@
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
 VERSION=$(mvn help:evaluate -Dexpression=project.version | grep -o "[0-9].[0-9]-SNAPSHOT$")
 set -x
-java -jar target/my-app-$VERSION.jar
+java -jar target/my-app-${$VERSION | grep -o "[0-9].[0-9]-SNAPSHOT$")}.jar

0cac5370d3e98a7d83fe315fda5ba205233363c1
Matthew Seagle <matthew.seagle@meresecure.com>
2018-07-24 13:42:45 -0700

    adding distinguishing lines

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
 VERSION=$(mvn help:evaluate -Dexpression=project.version | grep "[0-9].[0-9]-SNAPSHOT$")
+echo "BEFORE THE ECHO ---------------"
 echo $VERSION
+echo "AFTER THE ECHO ----------------"
 NAME=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.name}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
 java -jar target/my-app-1.0-SNAPSHOT.jar
-

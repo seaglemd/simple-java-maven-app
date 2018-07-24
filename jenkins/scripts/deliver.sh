4f06a92be2210a90854b5d5618c2abb67bba8fe4
Matthew Seagle <matthew.seagle@meresecure.com>
2018-07-24 14:54:10 -0700

    not extacting build numbers still

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -2,5 +2,5 @@
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
 VERSION=$(mvn help:evaluate -Dexpression=project.version | grep -o "[0-9].[0-9]-SNAPSHOT$")
 set -x
-build_number=$( $VERSION | grep -o "[0-9].[0-9]-SNAPSHOT$" )
+build_number=$($VERSION | grep -o "[0-9].[0-9]-SNAPSHOT$")
 java -jar target/my-app-$build_number.jar

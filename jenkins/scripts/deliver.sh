9b3098caf09179b3c1e919492a7ddf62cd125863
Matthew Seagle <matthew.seagle@meresecure.com>
2018-07-24 12:02:28 -0700

    more tests to try and figure out why it isn't able to access the target directory

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -25,6 +25,5 @@ echo ${VERSION}
 echo ${NAME}
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
-set -x
 java -jar target/${NAME}-${VERSION}.jar
 pwd

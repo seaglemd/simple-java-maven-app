0efa40f4edafa095c1166b26159b31ba4dd0a931
Matthew Seagle <matthew.seagle@meresecure.com>
2018-07-24 13:55:02 -0700

    trying to get grep to work

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
 VERSION=$(mvn help:evaluate -Dexpression=project.version | grep "[0-9].[0-9]-SNAPSHOT$")
-$(mvn help:evaluate -Dexpression=project.version | grep "[0-9].[0-9]-SNAPSHOT$" | grep "[0-9].[0-9]-SNAPSHOT$")
 echo "BEFORE THE ECHO ---------------"
 VERSION=$VERSION | grep "SNAPSHOT$"
-echo $VERSION
+echo $VERSION | grep "SNAPSHOT$"
 echo "AFTER THE ECHO ----------------"
 NAME=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.name}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
 java -jar target/my-app-1.0-SNAPSHOT.jar

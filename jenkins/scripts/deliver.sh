2d605bc88ea39e9b5535addcabfa8803933ad93c
Matthew Seagle <matthew.seagle@meresecure.com>
2018-07-24 13:56:07 -0700

    test grep

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -4,6 +4,7 @@ VERSION=$(mvn help:evaluate -Dexpression=project.version | grep "[0-9].[0-9]-SNA
 echo "BEFORE THE ECHO ---------------"
 VERSION=$VERSION | grep "SNAPSHOT$"
 echo $VERSION | grep "SNAPSHOT$"
+echo "fat bobby" | grep "bobby$"
 echo "AFTER THE ECHO ----------------"
 NAME=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.name}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
 java -jar target/my-app-1.0-SNAPSHOT.jar

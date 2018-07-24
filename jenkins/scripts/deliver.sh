6bf4fa9ab051e27699ac796ee1ea30a99b873877
Matthew Seagle <matthew.seagle@meresecure.com>
2018-07-24 14:22:53 -0700

    still trying to get the filename correct

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -2,11 +2,5 @@
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
 VERSION=$(mvn help:evaluate -Dexpression=project.version | grep -o "[0-9].[0-9]-SNAPSHOT$")
 NAME=$(mvn help:evaluate -Dexpression=project.name | grep -o "my-app")
-echo "BEFORE THE ECHO ---------------"
-echo $VERSION
-VERSION=$VERSION | grep -o "[0-9].[0-9]-SNAPSHOT$"
-echo $VERSION
-echo $NAME
-echo "AFTER THE ECHO ----------------"
-set +x
+set -x
 java -jar target/$NAME-$VERSION.jar

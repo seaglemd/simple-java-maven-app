d7528ac951f2b1f6f0a28247a7a18cf34cac7c65
Matthew Seagle <matthew.seagle@meresecure.com>
2018-07-24 12:12:36 -0700

    more java options for debugging

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -23,6 +23,7 @@ set +x
 set -x
 java -version
 set +x
+echo ${NAME}-${VERSION}.jar
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x

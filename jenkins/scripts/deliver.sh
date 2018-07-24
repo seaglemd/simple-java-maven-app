312fe78a443ef501a7e8cd5b0d0f218a3562dc6a
Matthew Seagle <matthew.seagle@meresecure.com>
2018-07-24 13:04:10 -0700

    still may not be setting the variables correctly, I may need another maven plugin

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -2,6 +2,9 @@
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
 
 VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
+echo $VERSION
+echo "NEXT VERSION -----------------------"
+echo ${VERSION}
 NAME=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.name}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
 java -jar target/${NAME}-${VERSION}.jar
 
